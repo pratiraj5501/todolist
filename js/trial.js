4866ccfc808b13ac39d487e8319521e657799492
pratiraj5501 <pratirajpatil1947@gmail.com>
2023-07-02 11:53:38 +0530

    liitle change to the index

--- js/trial.js
+++ js/trial.js
@@ -6,41 +6,41 @@
 //     ul.appendChild(Element);
 
 //  }
+//  function takeinput(){
+//     let lm=document.getElementById('inputs');
+//     console.log(lm);
+
+//  }
  
 function add_Task(){
-//     let myarray=['./images/icon.jpeg','./images/pink.png','./images/red.png'];
+    //  let myarray=['./images/icon.jpeg','./images/pink.png','./images/red.png'];
     let list=document.querySelector('.list');
     let ul =document.querySelector('.list ul')
-    console.log(ul);
-    let input= document.querySelector('.popup input');
-    console.log(input.innerHTML);
-
-//     document.createElement('img');
-let Element=document.createElement('li');
-Element.innerText="hi there";
-Element.style.color='red'
-// Element.classList.add('.list ')
-// console.log(Element);
-// ul.appendChild(Element);
-
-//     img.src=myarray[i];
-//     ul.appendChild()
-
-
-
+    let Element=document.createElement('li');
+    
+    // let input= document.querySelector('.popup input');
+    let input="checkme";
+    Element.innerHTML=input;
+ 
+// Element.appendChild('input') ;
  }
-//  
-
-let c=document.getElementById("c");
+   
+ 
 function display(){
     document.querySelector('.popup').style.display='flex';
 }
 
-function close(){
-    
-    console.log("hi there ");
-}
+ 
  function closeii(){
-    console.log("okkk");
     document.querySelector('.popup').style.display='none';
  }
+ 
+  function removoe(){
+     
+     let inputs= document.getElementById('inputs');
+     console.log(inputs.value);
+     add_Task();
+     ( document.querySelector('.popup').style.display='none');
+
+ }
+  
